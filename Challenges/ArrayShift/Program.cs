f9ee414bf7516526fe7f221d2e5889b33586dc9d
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-10 11:18:47 -0700

    Comments added

--- Challenges/ArrayShift/Program.cs
+++ Challenges/ArrayShift/Program.cs
@@ -6,34 +6,34 @@ namespace ArrayShift
     {
         static void Main(string[] args)
         {
-            int sampleInt1 = 5;
-            int[] sampleArr1 = {2,4,6,8};
+            int sampleInt1 = 5; //test int
+            int[] sampleArr1 = {2,4,6,8};//test array
 
-            ArrayStatic(sampleArr1, sampleInt1);
+            ArrayShift(sampleArr1, sampleInt1);//calls method that solves problem, returns an array
         }
 
-        static int[] ArrayStatic(int[] arr, int num)
+        static int[] ArrayShift(int[] arr, int num)
         {
-            int[] newArr = new int[arr.Length];
-            double middle = (arr.Length / 2);
+            int[] newArr = new int[arr.Length];//new array that solution is stored
+            double middle = (arr.Length / 2);//mid-point of array
 
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)//iterates through the old array
             {
-                if( i < middle)
+                if( i < Math.Ceiling(middle))//if below the mid-point it's replaced 1 to 1
                 {
                     newArr[i] = arr[i];
                 }
-                if (i == Math.Ceiling(middle))
+                if (i == Math.Ceiling(middle))// at mid-point the passed int is inserted
                 {
                     newArr[i] = num;
                 }
-                if (i > middle)
+                if (i > Math.Ceiling(middle))//remaining passed array ints are inserted into the new array
                 {
                     newArr[i] = arr[i - 1];
                 }
             }
 
-            return newArr;
+            return newArr;//returns solution in array
 
         }
     }
