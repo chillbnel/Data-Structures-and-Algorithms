3e633fbb8f25866d9074e6be57842fc7c47f887e
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-18 09:56:16 -0700

    FindKthValue Setup complete

---
+++ Challenges/FindKthValue/FindKthValue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindKthValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
