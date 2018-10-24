4c33d2764e079c5c8c1f6fdd118521d3c543a136
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-24 11:15:56 -0700

    Stack, Node, test framed out

---
+++ Challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOAnimalShelter.Classes
+{
+    public class Node
+    {
+        public object Value { get; set; } //changed from "int" to "object" to ensure that we're not bound to a certain type
+        public Node Next { get; set; }
+
+        public Node(object value)
+        {
+            Value = value;
+        }
+    }
+}
+
