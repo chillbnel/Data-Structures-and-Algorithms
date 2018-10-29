4b054e6e500d62a6a8994b01b16fedbd260274e7
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-29 10:17:22 -0700

    Templated out the Tree and Node classes

---
+++ Data-Structures/Tree/Tree/Classes/BinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    class BinaryTree
+    {
+        static void preOrder()
+        {
+
+        }
+
+        static void inOrder()
+        {
+
+        }
+
+        static void postOrder()
+        {
+
+        }
+    }
+}
