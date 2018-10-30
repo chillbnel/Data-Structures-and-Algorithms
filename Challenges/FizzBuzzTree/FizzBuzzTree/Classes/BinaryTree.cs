94bb5dfa559448e9b318eee41656f435979a7c7c
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-30 07:31:32 -0700

    Added to methods, application works without validation tests

---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Classes/BinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzTree.Classes
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+        public List<Node> nodes { get; set; }
+
+        public BinaryTree(Node root)
+        {
+            Root = root;
+        }
+
+        /// <summary>
+        /// Depth first transversal using the pre-order method
+        /// </summary>
+        /// <param name="root">the top node in a branch</param>
+        /// <returns>the transversed node</returns>
+        public List<Node> PreOrder(Node root)
+        {
+            nodes.Add(root);
+
+            if (root.LeftChild != null)
+            {
+                PreOrder(root.LeftChild);
+            }
+            if (root.RightChild != null)
+            {
+                PreOrder(root.RightChild);
+            }
+
+            return nodes;
+        }
+    }
+}
--- Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using FizzBuzzTree.Classes;
 
 namespace FizzBuzzTree
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node L1L2 = new Node(13, null, null);
+            Node L1R2 = new Node(15, null, null);
+            Node L1 = new Node(6, L1L2, L1R2);
+
+            Node R1L2 = new Node(3, null, null);
+            Node R1R2 = new Node(7, null, null);
+            Node R1 = new Node(5, R1L2, R1R2);
+
+            Node top = new Node(1, L1, R1);
+
+            BinaryTree BinaryTree = new BinaryTree(top);
+
+            FizzBuzzTree(BinaryTree, top);
+        }
+
+        /// <summary>
+        /// Tranverses the tree depth first, pre-orderly and prints the value of each node in the tree; if the node is divisible by 3, 5, or 15 then the value is replaced by "Fizz", "Buzz", or "FizzBuzz" respectively 
+        /// </summary>
+        /// <param name="tree">Binary tree to be traversed</param>
+        /// <param name="top">The tree's root value</param>
+        public static void FizzBuzzTree(BinaryTree tree, Node top)
+        {
+            tree.nodes = new List<Node>();
+            List<Node> preOrder = tree.PreOrder(top);
+
+            foreach(var item in preOrder)
+            {
+                Console.WriteLine(FizzBuzzPrint(item));
+            }
+        }
+
+        /// <summary>
+        /// Checks the value of a node in a series of conditional statements to determine if the value is divisble by 3, 5, or 15
+        /// </summary>
+        /// <param name="node">Node to be evaluated</param>
+        /// <returns>A string of either "Fizz", "Buzz", "FizzBuzz", or the Node's value</returns>
+        public static string FizzBuzzPrint(Node node)
+        {
+            if (node.Value % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (node.Value % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (node.Value % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return $"{node.Value}";
+             }
         }
     }
 }
