27cdee9679b4a0c77c7f3b3316d4e94a6839ed08
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-11-14 07:25:46 -0800

    linklist, node classes added

---
+++ Data-Structures/HashTables/HashTables/Classes/HashTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTables.Classes
+{
+    class HashTable
+    {
+        /// <summary>
+        /// Table with 1024 positions
+        /// </summary>
+        LinkedList[] Table = new LinkedList[1024];
+
+        public int GetHash(string key)
+        {
+            int HashIndex = 0;
+
+            foreach (char item in key)
+            {
+                HashIndex = HashIndex + (int)item;
+            }
+            HashIndex = (HashIndex * 599) / Table.Length;
+
+            return HashIndex;
+        }
+
+    }
+}
