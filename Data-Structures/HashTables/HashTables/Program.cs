cc531b3e69e2dd7eebe40438b5f9d80b2c205314
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-11-20 18:09:18 -0800

    added method to program, displays search results

--- Data-Structures/HashTables/HashTables/Program.cs
+++ Data-Structures/HashTables/HashTables/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using HashTables.Classes;
+using System;
 
 namespace HashTables
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHashTable();
+        }
+
+        public static void CreateHashTable()
+        {
+            HashTable table = new HashTable();
+
+            table.Add("brian", "love DSA");
+            Console.WriteLine("Add: brian, love DSA");
+            Console.WriteLine($"Found in Hash Table: {table.Contains("brian")}");
+
+            table.Add("I am", "6'4");
+            Console.WriteLine("Add: I am, 6'4");
+            Console.WriteLine($"Found in Hash Table: {table.Contains("I am")}");
+
+            table.Add("sonics", "2022 or bust");
+            Console.WriteLine("Add: sonics, 2022 or bust");
+            Console.WriteLine($"Found in Hash Table: {table.Contains("sonics")}");
+
+            table.Add("we I turn this in", "i'll be above 90%!");
+            Console.WriteLine("Add: we I turn this in, i'll be above 90%");
+            Console.WriteLine($"Found in Hash Table: {table.Contains("we I turn this in")}");
         }
     }
 }
