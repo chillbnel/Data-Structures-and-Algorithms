b806b5cf79736db6563e3a2a8994373433d468cb
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-12-05 13:09:05 -0800

    add a printout to the Program class

--- Data-Structures/Graph2/Graph2/Program.cs
+++ Data-Structures/Graph2/Graph2/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using Graph2.Classes;
 
 namespace Graph2
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GraphMethodsWork();
+        }
+        /// <summary>
+        /// Prints out the graph to the user
+        /// </summary>
+        public static void GraphMethodsWork()
+        {
+            Node node1 = new Graph2.Classes.Node("node1");
+            Node node2 = new Graph2.Classes.Node("node2");
+            Node node3 = new Graph2.Classes.Node("node3");
+            Node node4 = new Graph2.Classes.Node("node4");
+
+            Graph testGraph = new Graph(node1);
+
+            testGraph.AddEdge(node1, new Tuple<Node, int>(node2, 2));
+            testGraph.AddEdge(node2, new Tuple<Node, int>(node3, 3));
+            testGraph.AddEdge(node3, new Tuple<Node, int>(node4, 4));
+            testGraph.AddEdge(node4, new Tuple<Node, int>(node1, 1));
+
+            
+            Console.WriteLine("The Graph nodes:");
+            foreach (var node in testGraph.GetNodes())
+            {
+                Console.WriteLine(node.Value);
+            }
+
+            Console.WriteLine("The Graph neighbors for first node:");
+            foreach (var node in testGraph.GetNeighbors(node1))
+            {
+                Console.WriteLine(node.Value);
+            }
+
+            Console.WriteLine($"The Graph size: {testGraph.Size()}");
         }
     }
 }
