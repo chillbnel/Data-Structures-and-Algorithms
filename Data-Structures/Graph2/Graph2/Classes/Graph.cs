ca1c434b6903e888d29fb550da471375238a41e4
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-12-05 12:04:58 -0800

    AddAnEdge method created

--- Data-Structures/Graph2/Graph2/Classes/Graph.cs
+++ Data-Structures/Graph2/Graph2/Classes/Graph.cs
@@ -7,14 +7,35 @@ namespace Graph2.Classes
 {
     class Graph
     {
-        public Dictionary<Node, List<Node>> AdjacencyList { get; set; }
+        public Dictionary<Node, List<Tuple<Node, int>>> AdjacencyList { get; set; }
 
         public Graph(Node vertex)
         {
-            AdjacencyList = new Dictionary<Node, List<Node>>();
-            List<Node> neighbor = new List<Node>();
-            AdjacencyList.Add(vertex, neighbor);
+            AdjacencyList = new Dictionary<Node, List<Tuple<Node, int>>>();
+            List<Tuple<Node, int>> neighbors = new List<Tuple<Node, int>>();
+            AdjacencyList.Add(vertex, neighbors);
         }
 
+        public void AddAnEdge(Node parent, Tuple<Node, int> child)
+        {
+            if (!AdjacencyList.ContainsKey(parent))
+            {
+                List<Tuple<Node, int>> neighbors = new List<Tuple<Node, int>>();
+                AdjacencyList.Add(parent, neighbors);
+            }
+
+            var value = AdjacencyList.GetValueOrDefault(parent);
+            value.Add(child);
+
+            if (!AdjacencyList.ContainsKey(child.Item1))
+            {
+                List<Tuple<Node, int>> neighbors = new List<Tuple<Node, int>>();
+                AdjacencyList.Add(child.Item1, neighbors);
+            }
+
+            value = AdjacencyList.GetValueOrDefault(child.Item1);
+            value.Add(new Tuple<Node, int>(parent, child.Item2));
+
+        }
     }
 }
