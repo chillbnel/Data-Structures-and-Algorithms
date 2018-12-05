1b2ede861ea70ed5231538b3deeeb1513afd809f
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-12-05 11:12:29 -0800

    added graph class with reference and constructor

---
+++ Data-Structures/Graph2/Graph2/Classes/Graph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph2.Classes
+{
+    class Graph
+    {
+        public Dictionary<Node, List<Node>> AdjacencyList { get; set; }
+
+        public Graph(Node vertex)
+        {
+            AdjacencyList = new Dictionary<Node, List<Node>>();
+            List<Node> neighbor = new List<Node>();
+            AdjacencyList.Add(vertex, neighbor);
+        }
+
+    }
+}
